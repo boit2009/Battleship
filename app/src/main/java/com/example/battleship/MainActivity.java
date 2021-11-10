1d137e98a3d708a1880e4b3a48e2b1aa0d7fa00d
Botond Feczkó <feczko.botond@hallgato.ppke.hu>
2021-11-10 12:20:31 +0100

    first

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.battleship;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int INTERNET_PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView2);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Instantiate the RequestQueue.
+
+                String url ="https://localhost:8080/api/profile/1";
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.i("tag", response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("tag", error.getMessage());
+                    }
+                });
+                queue.add(stringRequest);
+            }
+        });
     }
+
 }
